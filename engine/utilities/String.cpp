b6424f2b6363a91e77a4854174599ca22de84738
astrand130 <alexstrand6622@protonmail.com>
2021-01-20 01:06:14 -0500

    String

--- engine/utilities/String.cpp
+++ engine/utilities/String.cpp
@@ -1,9 +1,131 @@
 #include "String.hpp"
 
-CT_UTILITIES_NAMESPACE::StringUtf8::StringUtf8()
-{
+#include "utf8/utf8.h"
+#define CUTE_UTF_IMPLEMENTATION
+#include "cute/cute_utf.h"
+
+ctStringUtf8::ctStringUtf8() {
+   _nullTerminate();
 }
 
-CT_UTILITIES_NAMESPACE::StringUtf8::StringUtf8(const char* input)
-{
+ctStringUtf8::ctStringUtf8(ctStringUtf8& str) {
+   _data = str._data;
+}
+
+ctStringUtf8::ctStringUtf8(const char* input, const size_t count) {
+   const size_t final_count = strnlen(input, count);
+   Reserve(count);
+   _data.Append(input, final_count);
+   _nullTerminate();
+}
+
+ctStringUtf8::ctStringUtf8(const char* input) {
+   const size_t count = strlen(input);
+   Reserve(count + 1);
+   _data.Append(input, count);
+   _nullTerminate();
+}
+
+ctResults ctStringUtf8::MapMemory(char* dest, size_t capacity) {
+   // Todo
+   return ctResults();
+}
+
+const char* ctStringUtf8::CStr() const {
+   return (const char*)_dataVoid();
+}
+
+void* ctStringUtf8::Data() {
+   return _dataVoid();
+}
+
+size_t ctStringUtf8::CodeLength() const {
+   return utf8len(_dataVoid());
+}
+
+size_t ctStringUtf8::ByteLength() const {
+   return strnlen(CStr(), Capacity());
+}
+
+size_t ctStringUtf8::Capacity() const {
+   return _data.Capacity();
+}
+
+ctResults ctStringUtf8::Reserve(const size_t amount) {
+   return _data.Reserve(amount);
+}
+
+void ctStringUtf8::Clear() {
+   _data.Clear();
+   _nullTerminate();
+}
+
+ctStringUtf8& ctStringUtf8::operator+=(const char c) {
+   _removeNullTerminator();
+   _data.Append(c);
+   _nullTerminate();
+   return *this;
+}
+
+ctStringUtf8& ctStringUtf8::operator+=(const int32_t c) {
+   _removeNullTerminator();
+   char tmp_buf[4] = {0, 0, 0, 0};
+   const size_t bytecount =
+     (size_t)((const char*)utf8catcodepoint(tmp_buf, c, 4) - tmp_buf);
+   _data.Append(tmp_buf, bytecount);
+   _nullTerminate();
+   return *this;
+}
+
+ctStringUtf8& ctStringUtf8::operator+=(const char* str) {
+   _removeNullTerminator();
+   _data.Append(str, strlen(str));
+   _nullTerminate();
+   return *this;
+}
+
+ctStringUtf8& ctStringUtf8::operator+=(const ctStringUtf8& str) {
+   _removeNullTerminator();
+   _data.Append(str.CStr(), str.ByteLength());
+   _nullTerminate();
+   return *this;
+}
+
+void ctStringUtf8::Printf(size_t max, const char* format, ...) {
+   const size_t beginning_length = ByteLength();
+   _removeNullTerminator();
+   _data.Append('\0', max);
+   va_list args;
+   va_start(args, format);
+   vsnprintf((char*)_dataVoid() + beginning_length, max - 1, format, args);
+   _removeNullTerminator(); /*Remove extra terminator*/
+   _nullTerminate();
+}
+
+ctStringUtf8& ctStringUtf8::ToUpper() {
+   utf8upr(_dataVoid());
+   return *this;
+}
+
+ctStringUtf8& ctStringUtf8::ToLower() {
+   utf8lwr(_dataVoid());
+   return *this;
+}
+
+inline void* ctStringUtf8::_dataVoid() const {
+   return (void*)_data.Data();
+}
+
+void* ctStringUtf8::_dataVoidOffset(size_t offset) const {
+   return (void*)&(_data.Data()[offset]);
+}
+
+void ctStringUtf8::_removeNullTerminator() {
+   while (_data[_data.Count() - 1] == '\0') {
+      _data.RemoveLast();
+   }
+}
+
+void ctStringUtf8::_nullTerminate() {
+   _data.Append('\0');
 }
--- engine/utilities/String.hpp
+++ engine/utilities/String.hpp
@@ -1,17 +1,45 @@
 #pragma once
 
-#include "DynamicArray.hpp"
+#include "Common.hpp"
 
-namespace CT_UTILITIES_NAMESPACE {
-
-class StringUtf8 {
+class ctStringUtf8 {
 public:
-	StringUtf8();
-	StringUtf8(const char* input);
-//Todo: tomorrow
-//codelength (amount of characters, not bytes) 
-//bytecount (amount of bytes)
+	ctStringUtf8();
+	ctStringUtf8(ctStringUtf8& str);
+	ctStringUtf8(const char* input, const size_t count);
+	ctStringUtf8(const char* input);
+
+	ctResults MapMemory(char* dest, size_t capacity);
+
+	const char* CStr() const;
+	void* Data();
+	size_t CodeLength() const;
+	size_t ByteLength() const;
+	size_t Capacity() const;
+	ctResults Reserve(const size_t amount);
+	void Clear();
+
+	/* ASCII Char Concat */
+	ctStringUtf8& operator+=(const char c);
+	/* Unicode Char Concat */
+	ctStringUtf8& operator+=(const int32_t c);
+	/* C String Concat */
+	ctStringUtf8& operator+=(const char* str);
+	/* StringUtf8 Concat */
+	ctStringUtf8& operator+=(const ctStringUtf8& str);
+
+	void Printf(size_t max, const char* format, ...);
+
+	ctStringUtf8& ToUpper();
+	ctStringUtf8& ToLower();
+
 private:
-	DynamicArray<char*> _data;
+	void* _dataVoid() const;
+	void* _dataVoidOffset(size_t offset) const;
+	void _removeNullTerminator();
+	void _nullTerminate();
+
+	size_t pStaticCapacity;
+	void* pStaticData;
+	ctDynamicArray<char> _data;
 };
-}
