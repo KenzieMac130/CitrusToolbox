ad37da5a39a77f30db9ed1bbaa5598e9b983d176
KenzieMac130 <kenziemac130@protonmail.com>
2021-09-14 12:10:11 -0400

    backup

--- engine/core/JobSystem.cpp
+++ engine/core/JobSystem.cpp
@@ -20,9 +20,6 @@
 #ifdef _WIN32
 #include <Windows.h>
 #endif
-#define CUTE_SYNC_IMPLEMENTATION
-#define CUTE_SYNC_SDL
-#include "cute/cute_sync.h"
 
 /* Stop windef's unwise macro definition from messing with Tracy */
 #ifdef _WIN32
@@ -34,61 +31,276 @@
 #include "EngineCore.hpp"
 #include "Settings.hpp"
 
-ctJobSystem::ctJobSystem(int32_t _threadReserve) {
+ctJobSystem* gJobSystem;
+
+#define CT_LONG_RUNNING_JOB 0x0001
+
+ctJobSystem::ctJobSystem(int32_t _threadReserve, bool shared) {
+   gJobSystem = this;
    threadReserve = _threadReserve;
-   threadCount = -1;
+   threadCountTarget = -1;
+   activeJobs = ctDynamicArray<JobInternal>();
+   threads = ctDynamicArray<ThreadInternal>();
+   memset(groups, 0, sizeof(groups[0]) * CT_MAX_JOB_GROUPS);
+   groupHandleManager = ctHandleManager();
+   jobLock = ctMutex();
+   threadLock = ctMutex();
 }
 
 ctResults ctJobSystem::Startup() {
    ZoneScoped;
-   ctSettingsSection* settings =
-     Engine->Settings->CreateSection("JobSystem", 1);
-   settings->BindInteger(&threadCount,
+   ctSettingsSection* settings = Engine->Settings->CreateSection("JobSystem", 1);
+   settings->BindInteger(
+     &threadReserve,
+     true,
+     true,
+     "ThreadReserve",
+     "Number of threads to set aside from the job pool under auto-select.");
+   settings->BindInteger(&threadCountTarget,
                          true,
                          true,
                          "ThreadCount",
                          "Number of threads to use for common jobs. (-1: Auto-select)");
+   settings->BindFloat(&longTermThreadPercent,
+                       true,
+                       true,
+                       "LongTermThreadPercent",
+                       "Percent of threads to use for long (possibly multi-frame) jobs.");
 
    int finalThreadCount = 1;
-   if (threadCount <= 0) {
+   if (threadCountTarget <= 0) {
       int coreCount = SDL_GetCPUCount();
       finalThreadCount = coreCount - threadReserve;
       if (finalThreadCount <= 0) { finalThreadCount = 1; }
    } else {
-      finalThreadCount = threadCount;
+      finalThreadCount = threadCountTarget;
    }
-   pool = cute_threadpool_create(finalThreadCount, NULL);
-   if (pool == NULL) {
-      ctFatalError(
-        -1, CT_NC("Failed to create threadpool!"));
-      return CT_FAILURE_UNSUPPORTED_HARDWARE;
+   threads.Reserve(finalThreadCount);
+   jobLock = ctMutexCreate();
+   threadLock = ctMutexCreate();
+   handleLock = ctMutexCreate();
+   for (int i = 0; i < finalThreadCount; i++) {
+      threads.Append(ThreadInternal());
+      ThreadInternal& ctx = threads.Last();
+      ctx.canHandleLongJobs = false;
+      ctx.pJobSystem = this;
+      ctx.wantsExit = false;
+      ctx.thread = ctThreadCreate(ThreadInternal::JobWorker, &ctx, "Job Worker");
    }
    ctDebugLog("Thread Pool: Reserved %d threads...", finalThreadCount);
    return CT_SUCCESS;
 }
 
 ctResults ctJobSystem::Shutdown() {
-   cute_threadpool_destroy(pool);
-   return CT_SUCCESS;
-}
-
-ctResults ctJobSystem::PushJob(void (*fpFunction)(void*), void* pData) {
-   cute_threadpool_add_task(pool, fpFunction, pData);
-   return CT_SUCCESS;
-}
-
-ctResults ctJobSystem::PushJobs(size_t count,
-                                void (**pfpFunction)(void*),
-                                void** ppData) {
-   for (size_t i = 0; i < count; i++) {
-      PushJob(pfpFunction[i], ppData[i]);
-   }
-   return CT_SUCCESS;
-}
-
-ctResults ctJobSystem::RunAllJobs() {
    ZoneScoped;
-   /* Todo: investigate, it seems to fail on scene ticks */
-   cute_threadpool_kick_and_wait(pool);
+   for (int32_t i = 0; i < threads.Count(); i++) {
+      ctMutexLock(threadLock);
+      threads[i].wantsExit = true;
+      ctMutexUnlock(threadLock);
+      ctThreadWaitForExit(threads[i].thread);
+   }
+   for (int32_t i = 0; i < CT_MAX_JOB_GROUPS; i++) {
+      if (groups[i].lock) { ctMutexDestroy(groups[i].lock); }
+   }
+   ctMutexDestroy(handleLock);
+   ctMutexDestroy(threadLock);
+   ctMutexDestroy(jobLock);
    return CT_SUCCESS;
+}
+
+ctJobGroupHandle ctJobSystem::CreateGroup(const char* name) {
+   ZoneScoped;
+   ctMutexLock(handleLock);
+   ctJobGroupHandle hndl = groupHandleManager.GetNewHandle();
+   ctMutexUnlock(handleLock);
+   const uint32_t idx = ctHandleGetIndex(hndl);
+   if (idx >= CT_MAX_JOB_GROUPS) { ctFatalError(-1, "Job system ran out of groups..."); }
+   GroupStateInternal& ctx = groups[idx];
+   ctx.lock = ctMutexCreate();
+   ctx.reference = 0;
+   strncpy(ctx.name, name, 51);
+   return hndl;
+}
+
+void ctJobSystem::DestroyGroup(ctJobGroupHandle groupHandle) {
+   ZoneScoped;
+   const uint32_t groupIdx = ctHandleGetIndex(groupHandle);
+   ctAssert(groupIdx < CT_MAX_JOB_GROUPS);
+   ctMutexLock(groups[groupIdx].lock);
+   ctMutexDestroy(groups[groupIdx].lock);
+   memset(&groups[groupIdx], 0, sizeof(groups[0]));
+   ctMutexUnlock(groups[groupIdx].lock);
+}
+
+ctResults ctJobSystem::PushJob(ctJobGroupHandle groupHandle,
+                               void (*fpFunction)(void*),
+                               void* pData,
+                               size_t dependencyCount,
+                               ctJobGroupHandle* pDependencies,
+                               bool longRunning) {
+   ZoneScoped;
+   return PushJobs(
+     groupHandle, 1, &fpFunction, &pData, dependencyCount, pDependencies, longRunning);
+}
+
+ctResults ctJobSystem::PushJobs(ctJobGroupHandle groupHandle,
+                                size_t count,
+                                void (**pfpFunction)(void*),
+                                void** ppData,
+                                size_t dependencyCount,
+                                ctJobGroupHandle* pDependencies,
+                                bool longRunning) {
+   ZoneScoped;
+   if (dependencyCount > 9) { return CT_FAILURE_INVALID_PARAMETER; }
+   ctMutexLock(jobLock);
+   for (size_t i = 0; i < count; i++) {
+      JobInternal newJob = JobInternal();
+      newJob.groupHandle = groupHandle;
+      newJob.fpFunction = pfpFunction[i];
+      newJob.pData = ppData[i];
+      newJob.dependencyCount = (uint16_t)dependencyCount;
+      if (longRunning) { newJob.flags |= CT_LONG_RUNNING_JOB; }
+      for (size_t j = 0; j < dependencyCount; j++) {
+         newJob.dependencies[j] = pDependencies[j];
+      }
+      activeJobs.Append(newJob);
+   }
+   // ctDebugLog("Add: %d", (int)activeJobs.Count());
+   ctMutexUnlock(jobLock);
+
+   const uint32_t groupIdx = ctHandleGetIndex(groupHandle);
+   ctAssert(groupIdx < CT_MAX_JOB_GROUPS);
+   ctMutexLock(groups[groupIdx].lock);
+   groups[groupIdx].reference += (int32_t)count;
+   ctMutexUnlock(groups[groupIdx].lock);
+   return CT_SUCCESS;
+}
+
+bool ctJobSystem::isFinished(ctJobGroupHandle groupHandle) {
+   ZoneScoped;
+   const uint32_t groupIdx = ctHandleGetIndex(groupHandle);
+   ctAssert(groupIdx < CT_MAX_JOB_GROUPS);
+   ctMutexLock(groups[groupIdx].lock);
+   bool finished = groups[groupIdx].reference <= 0;
+   ctMutexUnlock(groups[groupIdx].lock);
+   return finished;
+}
+
+void ctJobSystem::Wait(ctJobGroupHandle groupHandle) {
+   ZoneScoped;
+   while (!isFinished(groupHandle)) {
+      DoMoreWork(false);
+   };
+}
+
+void ctJobSystem::Wait(size_t groupCount, ctJobGroupHandle* pGroupHandles) {
+   ZoneScoped;
+   for (size_t i = 0; i < groupCount; i++) {
+      Wait(pGroupHandles[i]);
+   }
+}
+
+ctResults ctJobSystem::SetPriorityMode(ctJobSystemPriorityModes mode) {
+   ZoneScoped;
+   int numLongTermThreads = 1;
+   if (mode == CT_JOB_SYSTEM_PRIORITY_LOADING) {
+      numLongTermThreads = (int)threads.Count() - 1;
+   } else {
+      numLongTermThreads =
+        (int)ceil((float)threads.Count() * (longTermThreadPercent / 100));
+   }
+   if (numLongTermThreads < 0) { numLongTermThreads = 1; }
+   if (numLongTermThreads > threads.Count()) {
+      numLongTermThreads = (int)threads.Count();
+   }
+   ctMutexLock(threadLock);
+   for (int i = 0; i < threads.Count(); i++) {
+      if (numLongTermThreads > 0) {
+         threads[i].canHandleLongJobs = true;
+         numLongTermThreads--;
+      } else {
+         threads[i].canHandleLongJobs = false;
+      }
+   }
+   ctMutexUnlock(threadLock);
+   return CT_SUCCESS;
+}
+
+#include "imgui/imgui.h"
+void ctJobSystem::DebugImGui() {
+   ctMutexLock(jobLock);
+   ImGui::Text("Active Job Count %d", (int)activeJobs.Count());
+   ctMutexUnlock(jobLock);
+}
+
+void ctJobSystem::WaitForWork() {
+   bool result = false;
+   while (!result) {
+      ctMutexLock(jobLock);
+      result = activeJobs.isEmpty();
+      ctMutexUnlock(jobLock);
+   }
+}
+
+void ctJobSystem::DoMoreWork(bool longRunning) {
+   ZoneScoped;
+   JobInternal currentJob = JobInternal();
+   ctMutexLock(jobLock);
+   if (activeJobs.isEmpty()) {
+      ctMutexUnlock(jobLock);
+      return;
+   }
+   for (int i = (int)activeJobs.Count() - 1; i > 0; i--) {
+      const JobInternal job = activeJobs[i];
+      if (!longRunning && ctCFlagCheck(job.flags, CT_LONG_RUNNING_JOB)) { continue; }
+      bool depsOk = true;
+      for (int16_t j = 0; j < job.dependencyCount; j++) {
+         ctJobGroupHandle dep = job.dependencies[j];
+         if (!isFinished(dep)) {
+            depsOk = false;
+            break;
+         }
+      }
+      if (!depsOk) { continue; }
+      currentJob = job;
+      break;
+   }
+   ctMutexUnlock(jobLock);
+
+   if (!currentJob.fpFunction) {
+      ctDebugError("INVALID");
+      return;
+   }
+   currentJob.fpFunction(currentJob.pData);
+
+   const uint32_t groupIdx = ctHandleGetIndex(currentJob.groupHandle);
+   ctAssert(groupIdx < CT_MAX_JOB_GROUPS);
+   ctMutexLock(groups[groupIdx].lock);
+   groups[groupIdx].reference--;
+   ctMutexUnlock(groups[groupIdx].lock);
+   ctMutexLock(jobLock);
+   activeJobs.Remove(currentJob); /* BAD IDEA, DO SOMETHING BETTER! */
+   //ctDebugLog("Rem: %d", (int)activeJobs.Count());
+   ctMutexUnlock(jobLock);
+}
+
+ctJobSystem* ctGetJobSystem() {
+   ctAssert(gJobSystem);
+   return gJobSystem;
+}
+
+int ctJobSystem::ThreadInternal::JobWorker(void* data) {
+   ZoneScoped;
+   bool running = true;
+   ctJobSystem* pJobSystem = ((ThreadInternal*)data)->pJobSystem;
+   while (running) {
+      ctMutexLock(pJobSystem->threadLock);
+      ThreadInternal ctx = *(ThreadInternal*)data;
+      ctMutexUnlock(pJobSystem->threadLock);
+
+      running = !ctx.wantsExit;
+      // pJobSystem->WaitForWork();
+      pJobSystem->DoMoreWork(ctx.canHandleLongJobs);
+   }
+   return 0;
 }
--- engine/core/JobSystem.hpp
+++ engine/core/JobSystem.hpp
@@ -19,18 +19,91 @@
 #include "utilities/Common.h"
 #include "ModuleBase.hpp"
 
+enum ctJobSystemPriorityModes {
+   CT_JOB_SYSTEM_PRIORITY_REALTIME,
+   CT_JOB_SYSTEM_PRIORITY_LOADING,
+   CT_JOB_SYSTEM_PRIORITY_DEFAULT = CT_JOB_SYSTEM_PRIORITY_REALTIME
+};
+
+typedef ctHandle ctJobGroupHandle;
+
 class CT_API ctJobSystem : public ctModuleBase {
 public:
-   ctJobSystem(int32_t threadReserve);
+   ctJobSystem(int32_t threadReserve, bool shared = true);
    ctResults Startup() final;
    ctResults Shutdown() final;
 
-   ctResults PushJob(void (*fpFunction)(void*), void* pData);
-   ctResults PushJobs(size_t count, void (**pfpFunction)(void*), void** ppData);
-   ctResults RunAllJobs();
+/* https://blog.molecular-matters.com/2015/08/24/job-system-2-0-lock-free-work-stealing-part-1-basics/*/
+   ctJobGroupHandle CreateGroup(const char* name);
+   void DestroyGroup(ctJobGroupHandle groupHandle);
+   ctResults PushJob(ctJobGroupHandle groupHandle,
+                     void (*fpFunction)(void*),
+                     void* pData,
+                     size_t dependencyCount = 0,
+                     ctJobGroupHandle* pDependencies = NULL,
+                     bool longRunning = false);
+   ctResults PushJobs(ctJobGroupHandle groupHandle,
+                      size_t count,
+                      void (**pfpFunction)(void*),
+                      void** ppData,
+                      size_t dependencyCount = 0,
+                      ctJobGroupHandle* pDependencies = NULL,
+                      bool longRunning = false);
+   bool isFinished(ctJobGroupHandle groupHandle);
+   void Wait(ctJobGroupHandle groupHandle);
+   void Wait(size_t groupCount, ctJobGroupHandle* pGroupHandles);
+   ctResults SetPriorityMode(ctJobSystemPriorityModes mode);
+
+   void DebugImGui();
+
+   void WaitForWork();
+   void DoMoreWork(bool longRunning);
 
 protected:
    int32_t threadReserve;
-   int32_t threadCount;
-   struct cute_threadpool_t* pool;
+   int32_t threadCountTarget;
+   float longTermThreadPercent = 20.0f;
+
+   struct JobInternal {
+      bool operator==(const JobInternal& B) {
+         return memcmp(this, &B, sizeof(JobInternal)) == 0;
+      }
+      void (*fpFunction)(void*);
+      void* pData;
+      ctJobGroupHandle groupHandle;
+      int16_t flags;
+      int16_t dependencyCount;
+      ctJobGroupHandle dependencies[9];
+   };
+   static_assert(sizeof(JobInternal) == CT_ALIGNMENT_ALLOCATIONS,
+                 "JobInternal does not fit cache boundary");
+   ctDynamicArray<JobInternal> activeJobs;
+   ctMutex jobLock;
+
+   struct ThreadInternal {
+      ctThread thread;
+      ctJobSystem* pJobSystem;
+      int32_t canHandleLongJobs;
+      int32_t wantsExit;
+      char _pad[40];
+      static int JobWorker(void* ctx);
+   };
+   static_assert(sizeof(ThreadInternal) == CT_ALIGNMENT_ALLOCATIONS,
+                 "ThreadInternal does not fit cache boundary");
+   ctDynamicArray<ThreadInternal> threads;
+   ctMutex threadLock;
+
+   struct GroupStateInternal {
+      ctMutex lock;
+      int32_t reference;
+      char name[52];
+   };
+   static_assert(sizeof(GroupStateInternal) == CT_ALIGNMENT_ALLOCATIONS,
+                 "GroupStateInternal does not fit cache boundary");
+   GroupStateInternal groups[CT_MAX_JOB_GROUPS];
+
+   ctMutex handleLock;
+   ctHandleManager groupHandleManager;
 };
+
+ctJobSystem* ctGetJobSystem();
